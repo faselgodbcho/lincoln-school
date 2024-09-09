47092c6ffd06355f2697828c0884af3c16bf31a5
Fasel Godbcho <faselergando2000@gmail.com>
2024-09-09 15:02:35 -0500

    Complete the multi step form.

---
+++ src/components/MultiStepForm.jsx
@@ -0,0 +1,11 @@
+import FormInputs from "./FormInputs";
+
+const MultiStepForm = () => {
+  return (
+    <form className="min-h-[265px]">
+      <FormInputs />
+    </form>
+  );
+};
+
+export default MultiStepForm;
