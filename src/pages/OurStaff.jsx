7d6ebbbecb2d30df1804b1cd0a7713c68b666ea9
Fasel Godbcho <faselergando2000@gmail.com>
2024-09-28 05:57:57 -0500

    Debug

--- src/pages/OurStaff.jsx
+++ src/pages/OurStaff.jsx
@@ -20,15 +20,15 @@ const OurStaff = () => {
 
           <div data-aos="fade-left" className="md:w-1/2 pt-4 md:pt-8">
             <figure>
-              <blockquote class="pl-8 pr-8 py-12 relative">
-                <p class="text-left mt-2 text-primary before:content-['\201C'] before:text-[35px] after:text-[35px] before:font-serif before:absolute before:top-4 before:left-0 before:transform before:translate-x-2 before:translate-y-2 after:content-['\201D'] after:font-serif after:absolute after:-bottom-1 after:right-7 after:transform after:-translate-x-2 after:-translate-y-2">
+              <blockquote className="pl-8 pr-8 py-12 relative">
+                <p className="text-left mt-2 text-primary before:content-['\201C'] before:text-[35px] after:text-[35px] before:font-serif before:absolute before:top-4 before:left-0 before:transform before:translate-x-2 before:translate-y-2 after:content-['\201D'] after:font-serif after:absolute after:-bottom-1 after:right-7 after:transform after:-translate-x-2 after:-translate-y-2">
                   We aim to improve school quality by supporting pupils in line
                   with Ethiopian Education and Training Policy. We aim to
                   improve school quality by supporting pupils in line with
                   Ethiopian Education and Training Policy.
                 </p>
               </blockquote>
-              <figcaption class="italic text-right mt-2 text-primary">
+              <figcaption className="italic text-right mt-2 text-primary">
                 &#8212;Alexander the first.
               </figcaption>
             </figure>
@@ -38,15 +38,15 @@ const OurStaff = () => {
         <div className="text-center mt-4 md:text-left w-full lg:container mx-auto flex flex-col-reverse sm:flex-row md:gap-24 justify-center py-8 pt-12 px-6">
           <div data-aos="fade-right" className="md:w-1/2 pt-4 md:pt-8">
             <figure>
-              <blockquote class="pl-8 pr-8 py-12 relative">
-                <p class="text-left mt-2 text-primary before:content-['\201C'] before:text-[35px] after:text-[35px] before:font-serif before:absolute before:top-4 before:left-0 before:transform before:translate-x-2 before:translate-y-2 after:content-['\201D'] after:font-serif after:absolute after:-bottom-1 after:right-7 after:transform after:-translate-x-2 after:-translate-y-2">
+              <blockquote className="pl-8 pr-8 py-12 relative">
+                <p className="text-left mt-2 text-primary before:content-['\201C'] before:text-[35px] after:text-[35px] before:font-serif before:absolute before:top-4 before:left-0 before:transform before:translate-x-2 before:translate-y-2 after:content-['\201D'] after:font-serif after:absolute after:-bottom-1 after:right-7 after:transform after:-translate-x-2 after:-translate-y-2">
                   We aim to improve school quality by supporting pupils in line
                   with Ethiopian Education and Training Policy. We aim to
                   improve school quality by supporting pupils in line with
                   Ethiopian Education and Training Policy.
                 </p>
               </blockquote>
-              <figcaption class="italic text-right mt-2 text-primary">
+              <figcaption className="italic text-right mt-2 text-primary">
                 &#8212;Alexander the first.
               </figcaption>
             </figure>
@@ -72,15 +72,15 @@ const OurStaff = () => {
           </div>
           <div data-aos="fade-left" className="md:w-1/2 pt-4 md:pt-8">
             <figure>
-              <blockquote class="pl-8 pr-8 py-12 relative">
-                <p class="text-left mt-2 text-primary before:content-['\201C'] before:text-[35px] after:text-[35px] before:font-serif before:absolute before:top-4 before:left-0 before:transform before:translate-x-2 before:translate-y-2 after:content-['\201D'] after:font-serif after:absolute after:-bottom-1 after:right-7 after:transform after:-translate-x-2 after:-translate-y-2">
+              <blockquote className="pl-8 pr-8 py-12 relative">
+                <p className="text-left mt-2 text-primary before:content-['\201C'] before:text-[35px] after:text-[35px] before:font-serif before:absolute before:top-4 before:left-0 before:transform before:translate-x-2 before:translate-y-2 after:content-['\201D'] after:font-serif after:absolute after:-bottom-1 after:right-7 after:transform after:-translate-x-2 after:-translate-y-2">
                   We aim to improve school quality by supporting pupils in line
                   with Ethiopian Education and Training Policy. We aim to
                   improve school quality by supporting pupils in line with
                   Ethiopian Education and Training Policy.
                 </p>
               </blockquote>
-              <figcaption class="italic text-right mt-2 text-primary">
+              <figcaption className="italic text-right mt-2 text-primary">
                 &#8212;Alexander the first.
               </figcaption>
             </figure>
@@ -90,15 +90,15 @@ const OurStaff = () => {
         <div className="text-center mt-4 md:text-left w-full lg:container mx-auto flex flex-col-reverse sm:flex-row md:gap-24 justify-center py-8 pt-12 px-6">
           <div data-aos="fade-right" className="md:w-1/2 pt-4 md:pt-8">
             <figure>
-              <blockquote class="pl-8 pr-8 py-12 relative">
-                <p class="text-left mt-2 text-primary before:content-['\201C'] before:text-[35px] after:text-[35px] before:font-serif before:absolute before:top-4 before:left-0 before:transform before:translate-x-2 before:translate-y-2 after:content-['\201D'] after:font-serif after:absolute after:-bottom-1 after:right-7 after:transform after:-translate-x-2 after:-translate-y-2">
+              <blockquote className="pl-8 pr-8 py-12 relative">
+                <p className="text-left mt-2 text-primary before:content-['\201C'] before:text-[35px] after:text-[35px] before:font-serif before:absolute before:top-4 before:left-0 before:transform before:translate-x-2 before:translate-y-2 after:content-['\201D'] after:font-serif after:absolute after:-bottom-1 after:right-7 after:transform after:-translate-x-2 after:-translate-y-2">
                   We aim to improve school quality by supporting pupils in line
                   with Ethiopian Education and Training Policy. We aim to
                   improve school quality by supporting pupils in line with
                   Ethiopian Education and Training Policy.
                 </p>
               </blockquote>
-              <figcaption class="italic text-right mt-2 text-primary">
+              <figcaption className="italic text-right mt-2 text-primary">
                 &#8212;Alexander the first.
               </figcaption>
             </figure>
